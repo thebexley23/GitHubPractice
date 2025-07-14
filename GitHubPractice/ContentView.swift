9371695705952324284c753e9d9a614d473b17c7
Lily Zhang <lily@kodewithklossy.com>
2025-07-14 09:55:52 -0500

    added "change 2"

--- GitHubPractice/ContentView.swift
+++ GitHubPractice/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Change 1")
+            Text("Lily's Change Two")
         }
         .padding()
     }
