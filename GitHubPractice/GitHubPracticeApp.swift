04e2de5f96c05f59be2cb57a864e57d865479c55
Eunah <eunahmaumbe@gmail.com>
2025-07-14 09:38:51 -0500

    Initial Commit

---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by Scholar on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
