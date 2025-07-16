238f91c3e780bec661be6606b7f6793c7e3fc277
Lily Zhang <lily@kodewithklossy.com>
2025-07-16 13:41:03 -0500

    homescreen again

--- GitHubPractice/SwiftUIView.swift
+++ GitHubPractice/SwiftUIView.swift
@@ -1,18 +1,64 @@
-//
-//  SwiftUIView.swift
-//  GitHubPractice
-//
-//  Created by Scholar on 7/14/25.
-//
-
 import SwiftUI
 
-struct SwiftUIView: View {
+struct CalendarView: View {
+    
+    @State private var selectedDate = Date()
+    @State private var path = NavigationPath()
+    
+    @StateObject private var viewModel = SessionViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $path) {
+            VStack(spacing: 20) {
+                HStack {
+                    Text("Schedule LockIn Session")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
+                Spacer()
+                DatePicker("", selection: $selectedDate, in: Date()..., displayedComponents: [.date])
+                    .datePickerStyle(.graphical)
+                    .padding()
+                
+                Button("Create Session") {
+                    path.append(selectedDate)
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                
+                Button("Scheduled Sessions") {
+                    path.append("scheduledList")
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                Spacer()
+                Spacer()
+            }
+            .padding()
+            .background(Color(red: 153/255, green: 224/255, blue: 168/255))
+            .navigationTitle("Home")
+            .navigationBarHidden(true)
+            .navigationDestination(for: Date.self) { date in
+                ScheduleView(date: date)
+                    .environmentObject(viewModel)
+            }
+            .navigationDestination(for: String.self) { value in
+                if value == "scheduledList" {
+                    ScheduledListView()
+                        .environmentObject(viewModel)
+                }
+            }
+        }
     }
 }
 
+
 #Preview {
-    SwiftUIView()
+    CalendarView()
 }
+
