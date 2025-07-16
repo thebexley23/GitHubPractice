492f26c3882d17a9781e9ef508ff8f33495cb78c
Lily Zhang <lily@kodewithklossy.com>
2025-07-16 12:55:03 -0500

    homescreen + choosing times

---
+++ SessionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SessionViewModel.swift
+//  GitHubPractice
+//
+//  Created by Scholar on 7/16/25.
+//
+
+import Foundation
---
+++ TimeList.swift
@@ -0,0 +1,18 @@
+//
+//  TimeList.swift
+//  GitHubPractice
+//
+//  Created by Scholar on 7/16/25.
+//
+
+import SwiftUI
+
+struct TimeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeList()
+}
