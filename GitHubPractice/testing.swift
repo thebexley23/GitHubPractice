5e96e59b0bd3de60ea596e67ecf5a70cd2948a9e
Lily Zhang <lily@kodewithklossy.com>
2025-07-17 14:11:51 -0500

    blank file

---
+++ GitHubPractice/testing.swift
@@ -0,0 +1,18 @@
+//
+//  testing.swift
+//  GitHubPractice
+//
+//  Created by Scholar on 7/17/25.
+//
+
+import SwiftUI
+
+struct testing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testing()
+}
